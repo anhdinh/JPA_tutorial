351d51a6e172124ef4b9d203621de941fed54b0f
Anh Dinh <dta89uct@gmail.com>
2021-03-10 23:28:03 +0700

    change name function

---
+++ string.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int change_the_name(char *s)
+{
+   char *p = s;
+   
+   
+   
+   
+   //this is a comment
+
+   while (*p)
+      ++p;
+
+   return (p - s);
+}
+
+int main(void)
+{
+   int i;
+   char *s[] = 
+   {
+      "Git tutorials",
+      "Tutorials Point"
+   };
+
+   for (i = 0; i < 2; ++i)
+      
+   printf("string lenght of %s = %d\n", s[i], change_the_name(s[i]));
+
+   return 0;
+}
